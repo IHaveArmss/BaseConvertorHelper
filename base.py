d0bd2852205ffc773eab25d2f7eadf5e5c0eb96d
Ayan <ayanbar2017@gmail.com>
2025-03-28 22:09:08 +0200

    clearer labels + functioning scroll

--- base.py
+++ base.py
@@ -21,10 +21,11 @@ innerFrame.place(x=0,y=200)
 BaseFrom = ttk.Combobox(root,width=20,height=20,values=[str(i) for i in range(2,17)])
 BaseFrom.place(x=125,y=50)
 
-canvas =Canvas(innerFrame, bg="blue",width=360, height=180)
+canvas =Canvas(innerFrame,width=360, height=180)
 scrollbar= Scrollbar(innerFrame, orient=VERTICAL, command=canvas.yview)
-scrollableFrame = Frame(canvas,bg="blue")
+scrollableFrame = Frame(canvas)
 canvas.create_window((0, 0), window=scrollableFrame, anchor="nw")
+
 canvas.configure(yscrollcommand=scrollbar.set)
 scrollbar.pack(side=RIGHT, fill=Y)
 canvas.pack(side=LEFT, fill=BOTH, expand=True)
@@ -53,27 +54,33 @@ def ConvertBase():
         
     y_offset = 10
     s=0
-    Label(scrollableFrame, text=f"Converting {number} from base {baseF} to base 10:", fg="white", bg="blue").pack(anchor="w", padx=10)
-    for i in range(len(number)):
+    Label(scrollableFrame, text=f"Converting {number} from base {baseF} to base 10:" ).pack(anchor="w", padx=50)
+    for i in range(len(number)):# transforma in baza 10
+        digi = int(number[i],base=int(baseF))
+        term =digi *pow(int(baseF), len(number)-i-1)
         s += int(number[i], base=int(baseF)) * pow(int(baseF), len(number) - i - 1)
+        Label(scrollableFrame, text=f"{digi} × {baseF}^{len(number) - i - 1} = {term}",  ).pack(anchor="w", padx=50)
 
-    Label(scrollableFrame, text=f"Base 10 equivalent: {s}", fg="yellow", bg="blue").pack(anchor="w", padx=10)
+    Label(scrollableFrame, text=f"Base 10 equivalent: {s}").pack(anchor="w", padx=50)
     convert = []
     module = 0
-    Label(scrollableFrame, text=f"Converting {s} from base 10 to base {baseT}:", fg="white", bg="blue").pack(anchor="w", padx=10)
+
+    Label(scrollableFrame, text=f"Converting {s} from base 10 to base {baseT}:").pack(anchor="w", padx=50)
     while s > 0 :
         module = s % int(baseT)
         s = s // int(baseT)
-        convert.append(module)
-        Label(scrollableFrame, text=f"Divide by {baseT}: Rest = {module}, Cat = {s}", fg="white", bg="blue").pack(anchor="w", padx=10)
+        convert.append(module)#descompune numarul in baza 10 in baza T
+        Label(scrollableFrame, text=f"Divide by {baseT}: Rest = {module}, Cat = {s}").pack(anchor="w", padx=50)
 
     finalNumber = 0
     p=1
 
     for i in range(len(convert)):
-        finalNumber += p * convert[i]
+        finalNumber += p * convert[i]# afiseaza corect numarul in ordine inversa 
         p = p * 10
-    Label(scrollableFrame, text=f"Final result in base {baseT}: {finalNumber}", fg="yellow", bg="blue").pack(anchor="w", padx=10)
+    Label(scrollableFrame, text=f"Final result in base {baseT}: {finalNumber}").pack(anchor="w", padx=50)
+    updateRegion()
+
     print (finalNumber)
      
         
